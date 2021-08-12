cea68653152235816ac8fffdf54e2a98c71e7b10
NGOVANTIEN <ngotienpts@gmail.com>
2021-08-12 08:59:29 +0700

    12/8/2021

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
-var clickElement = document.querySelectorAll(".box");
-for (var i = 0; i < clickElement.length; i++) {
-    var firstElement = clickElement[i].firstElementChild;
-    firstElement.onclick = function(e) {
-        e.target.nextElementSibling.classList.toggle("div1");
+let parentElement = document.querySelectorAll(".container");
+for (let i = 0; i < parentElement.length; i++) {
+    let clickElement = parentElement[i].querySelector(".box");
+    let childElement = clickElement.querySelector(".box1");
+    childElement.onclick = function(e) {
+        e.target.parentElement.nextElementSibling.classList.toggle("div1");
     };
 }
+console.log([parentElement]);
